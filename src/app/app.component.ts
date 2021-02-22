146c52dd327265b93576483807a77fede3a81a35
Ihor Koval <nicholakoval@gmail.com>
2021-02-22 11:50:50 +0200

    steko-web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'steko-web';
+  isDisplay = false;
+  Images = [
+    '../assets/images/slide1.jpg',
+    '../assets/images/slide2.jpg',
+    '../assets/images/slide3.jpg',
+    '../assets/images/slide4.jpg',
+    '../assets/images/slide5.jpg',
+    '../assets/images/slide6.jpg',
+    '../assets/images/slide7.jpg',
+    '../assets/images/slide8.jpg',
+  ];
+
+  SlideOptions = { items: 1, dots: true, nav: false, loop: true, autoplay: true };
+  CarouselOptions = { items: 3, dots: true, nav: false };
+
+  showMenu() {
+    this.isDisplay = !this.isDisplay;
+    console.log('menu');
+
+  }
+
+
 }
